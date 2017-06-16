be19759f18fab8b1d8fbdee80cbed634ab950ff8
Igor Gorlov <gorlov35@gmail.com>
2017-06-16 21:31:08 +0300

    almost complete main

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,22 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'slick-carousel';
 
-$(() => {
+(function ($) {
 	svg4everybody();
-});
+
+
+	$(function () {
+		const hSlider = $('.hero__slider');
+		hSlider.slick({
+			arrows: false,
+			dots: true
+		});
+
+	});
+
+})(jQuery);
+
+
+
+
